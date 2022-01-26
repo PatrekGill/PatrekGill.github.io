3c87932c18ccd6a2f5ec6ccf54bf9f6c75798207
Ansible2 <afoshark@yahoo.com>
2022-01-25 22:52:46 -0700

    started on sidebar for site

---
+++ sidebar/sidebar.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  	const showNavbar = (toggleButtonId, sideBarId, bodyId, headerId) => {
+    	const toggleButton = document.getElementById(toggleButtonId),
+		sideBar = document.getElementById(sideBarId),
+		bodypd = document.getElementById(bodyId),
+		headerpd = document.getElementById(headerId);
+
+		// Validate that all variables exist
+		if (toggleButton && sideBar && bodypd && headerpd) {
+			toggleButton.addEventListener("click", () => {
+				// show sidebar
+				sideBar.classList.toggle("show");
+				// change icon
+				toggleButton.classList.toggle("bx-x");
+				// add padding to body
+				bodypd.classList.toggle("body-pd");
+				// add padding to header
+				headerpd.classList.toggle("body-pd");
+			});
+		}
+  	};
+
+  	showNavbar("header-toggle", "side-bar", "body-pd", "header");
+
+  	/*===== LINK ACTIVE =====*/
+  	const linkColor = document.querySelectorAll(".nav_link");
+
+  	function colorLink() {
+		if (linkColor) {
+			linkColor.forEach((l) => l.classList.remove("active"));
+			this.classList.add("active");
+		}
+ 	}
+  	linkColor.forEach((l) => l.addEventListener("click", colorLink));
+
+  	// Your code to run since DOM is loaded and ready
+});
