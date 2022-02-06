07c5f5061983c974ce93bece3ad7dd987c6aaab0
PatrekGill <patrekGill@yahoo.com>
2022-02-05 20:17:47 -0700

    added popover for profile links

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -63,4 +63,9 @@ function calculateCollapsedScale(item) {
 	  x: collapsed.width / expanded.width,
 	  y: collapsed.height / expanded.height
 	};
-  }
+}
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  	return new bootstrap.Popover(popoverTriggerEl)
+})
