71bec73666d5105014631822a0cc547cdaa378a1
PatrekGill <patrekGill@yahoo.com>
2022-02-08 23:07:00 -0700

    fixed alternating background colors

--- assets/JS/sidebar.js
+++ assets/JS/sidebar.js
@@ -79,3 +79,13 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   	return new bootstrap.Popover(popoverTriggerEl)
 })
+
+
+window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+});
