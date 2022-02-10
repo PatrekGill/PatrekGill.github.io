3a5eb83fdaf8da0f6d8e2d4bc26d6aae130be117
PatrekGill <patrekGill@yahoo.com>
2022-02-09 22:17:20 -0700

    converted JSON files to JS and added to HTML

---
+++ assets/JS/init.js
@@ -0,0 +1,18 @@
+console.log(skills);
+
+// Enable Bootstrap popovers
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  	return new bootstrap.Popover(popoverTriggerEl)
+});
+
+
+// Enable Animated On Scroll
+window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+});
--- assets/JS/sidebar.js
+++ assets/JS/sidebar.js
@@ -72,20 +72,3 @@ function PORT_fnc_expandSideBar(toggleButtonId, sideBarId, bodyId, headerId) {
 		});
 	}
 }
-
-
-// Enable Bootstrap popovers
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  	return new bootstrap.Popover(popoverTriggerEl)
-})
-
-
-window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
-});
