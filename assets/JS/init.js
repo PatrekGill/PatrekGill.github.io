adc11a97f7af45b9926fb376a3116c1791471545
PatrekGill <patrekGill@yahoo.com>
2022-02-10 00:33:20 -0700

    added progress bars but in a broken styling state

--- assets/JS/init.js
+++ assets/JS/init.js
@@ -1,5 +1,3 @@
-console.log(skills);
-
 // Enable Bootstrap popovers
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
@@ -15,4 +13,44 @@ window.addEventListener('load', () => {
       once: true,
       mirror: false
     })
+});
+
+window.addEventListener('load', () => {
+	const skillBarsDiv_right = document.getElementById("skillBarsDivRightColumn_id");
+	const skillBarsDiv_left = document.getElementById("skillBarsDivLeftColumn_id");
+	let useLeft = true; 
+	function getBarColumnDiv() {
+		let div = null;
+		if (useLeft) {
+			div = skillBarsDiv_left
+		} else {
+			div = skillBarsDiv_right;
+		};
+
+		useLeft = !useLeft;
+		return div;
+	};
+
+
+	skills.forEach(
+		(skill) => {
+			const columnDiv = getBarColumnDiv();
+			const progressDiv = document.createElement("div");
+			progressDiv.classList.add("progress");
+			columnDiv.appendChild(progressDiv);
+
+			const barDiv = document.createElement("div");
+			barDiv.classList.add("progress-bar");
+			barDiv.setAttribute("aria-valuenow","25");
+			barDiv.setAttribute("aria-valuemax","100");
+			barDiv.setAttribute("aria-valuemin","0");
+			barDiv.setAttribute("role","progressbar");
+
+			barDiv.style = "width: 25%";
+			barDiv.textContent = "25%";
+
+			progressDiv.appendChild(barDiv);
+			
+		}
+	);
 });
