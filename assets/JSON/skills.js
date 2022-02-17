e3a353ccd56f0191deb1c3c9ff425bb6ad9a8994
PatrekGill <patrekGill@yahoo.com>
2022-02-17 11:43:51 -0700

    added more skills

--- assets/JSON/skills.js
+++ assets/JSON/skills.js
@@ -20,5 +20,26 @@ const skills =
 		"icon": "fa-brands fa-java",
 		"description": "Java (8) was the primary subject of my coding bootcamp, Skill Distillery.",
 		"rating": 50
+	},
+	{
+		"id": 3,
+		"title": "C#",
+		"icon": "icons8 icons8-c-sharp",
+		"description": "While not something I use regularly, I have dabbled in C# and created a project for Unity with it.",
+		"rating": 30
+	},
+	{
+		"id": 4,
+		"title": "C++",
+		"icon": "icons8 icons8-c-plus-plus",
+		"description": "",
+		"rating": 20
+	},
+	{
+		"id": 5,
+		"title": "Spring Boot",
+		"icon": "icons8 icons8-spring-logo",
+		"description": "",
+		"rating": 50
 	}
 ];
