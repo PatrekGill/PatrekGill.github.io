7ed8b051725f298c3149eada56f0fa98bc4b57c1
PatrekGill <patrekGill@yahoo.com>
2022-02-17 00:11:33 -0700

    adjusted skills JSON

--- assets/JSON/skills.js
+++ assets/JSON/skills.js
@@ -5,20 +5,20 @@ const skills =
 		"title": "HTML",
 		"icon": "fa-brands fa-html5",
 		"description": "",
-		"rating": 40
+		"rating": 70
 	},
 	{
 		"id": 1,
 		"title": "CSS",
 		"icon": "fa-brands fa-css3-alt",
-		"description": "",
-		"rating": 20
+		"description": "CSS has been a challenge to feel as though I grasp it enough to teach another person effectively. However, it remains my goal to become 'fluent' in it and others such as SASS and LESS",
+		"rating": 30
 	},
 	{
 		"id": 2,
 		"title": "Java",
 		"icon": "fa-brands fa-java",
-		"description": "",
+		"description": "Java (8) was the primary subject of my coding bootcamp, Skill Distillery.",
 		"rating": 50
 	}
 ];
